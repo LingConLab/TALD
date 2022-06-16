9daaf358708027f3e877f4eec0524addca3d62e4
George Moroz <agricolamz@gmail.com>
2022-06-16 06:19:47 +0300

    correct village dialect names

--- compile_website.R
+++ compile_website.R
@@ -140,7 +140,7 @@ Visualization 1 (**Extrapolated data**) and 3 (**General datapoints**) are both 
 ",
 str_c('feature_dataset <- read_tsv("../orig_table/', 
       str_remove(str_remove(i, "_map.Rmd"), "\\d{1,}_"),
-      '.tsv")'),
+      '.tsv)'),
 "
 
 feature_dataset %>% 
