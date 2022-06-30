d41b09ab680e924f48998985e3a76b69a5bddcbf
George Moroz <agricolamz@gmail.com>
2022-06-30 08:52:22 +0300

    update adpositions

---
+++ docs/tests/test-hashes.R
@@ -0,0 +1,19 @@
+library(tidyverse)
+library(testthat)
+
+test_that("Test hashes in rmarkdown text:
+I expect that the h1 header `#` is absent in Rmd files.", {
+  
+  n_wrong_hashes <- map_dbl(list.files("../data/orig_rmd", full.names = TRUE), function(rmd){
+    sum(str_detect(read_lines(rmd, progress = FALSE), "^# ")) 
+  })
+  
+  observed <- str_c(file = list.files("../data/orig_rmd"), 
+                    ": ",
+                    n_wrong_hashes = n_wrong_hashes, 
+                    " wrong hashes")
+  expected <- str_c(file = list.files("../data/orig_rmd"), 
+                    ": 0 wrong hashes")
+  
+  expect_equal(observed, expected)
+})
