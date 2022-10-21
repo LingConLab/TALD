cb142274860a79fb38f768ccb18fd207e9625277
George Moroz <agricolamz@gmail.com>
2022-10-21 09:21:45 +0300

    test pre-commit

--- code/test_and_write_logs.R
+++ code/test_and_write_logs.R
@@ -4,5 +4,5 @@ suppressPackageStartupMessages(library(tidyverse))
 test_logs <- read_lines("test_logs.txt")
 test_logs[str_detect(test_logs, "(everything is ok)|(0 wrong hashes)", negate = TRUE)] %>% 
   write_lines("test_logs.txt", append = FALSE)
-read_lines("test_logs.txt") %>% cat()
+read_lines("test_logs.txt") %>% cat(sep = "\n")
 system("git add -u")
