b8e0dcac9ae7aaadcc27cd7a8858daa829c88092
George Moroz <agricolamz@gmail.com>
2023-02-11 19:45:26 +0300

    fix Maksim's root of hundred value

--- docs/code/test_and_write_logs.R
+++
@@ -1,8 +0,0 @@
-file.remove("test_logs.txt")
-testthat::test_dir("tests", stop_on_failure = FALSE)
-suppressPackageStartupMessages(library(tidyverse))
-test_logs <- read_lines("test_logs.txt")
-test_logs[str_detect(test_logs, "(everything is ok)|(0 wrong hashes)", negate = TRUE)] %>% 
-  write_lines("test_logs.txt", append = FALSE)
-read_lines("test_logs.txt") %>% cat(sep = "\n")
-system("git add -u")
