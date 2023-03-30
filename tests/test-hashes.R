5dc1b0e0852dfda102c9cfb02b16f24d37861f65
George Moroz <agricolamz@gmail.com>
2023-03-30 15:19:44 +0300

    update biblib

--- tests/test-hashes.R
+++ tests/test-hashes.R
@@ -13,7 +13,7 @@ I expect that the h1 header `#` is absent in Rmd files.", {
                     n_wrong_hashes = n_wrong_hashes, 
                     "          wrong hashes")
   expected <- str_c(file = list.files("../data/orig_rmd"), 
-                    ": 0 wrong hashes")
+                    ":          0          wrong hashes")
   
   write_lines(observed, "../test_logs.txt", append = TRUE)
   expect_equal(observed, expected)
