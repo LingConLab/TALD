19c79d3679bc9a460083c74cdab9eb68774eeaea
George Moroz <agricolamz@gmail.com>
2023-05-04 15:20:30 +0300

    update

---
+++ docs/tests/test-spelling.R
@@ -0,0 +1,25 @@
+library(tidyverse)
+library(testthat)
+library(spelling)
+
+test_that("Test spellingin chapters", {
+  
+  ignore_spelling <- read_lines("../data/spelling_exceptions.txt")
+  files_to_check <- c(list.files("../data/orig_rmd", full.names = TRUE),
+                      "../README.md", "../about.Rmd", "../team.Rmd", 
+                      "../index.Rmd", "../changes.Rmd")
+  
+  wrong_spelling <- map_dfr(files_to_check[-69], function(rmd){
+    spelling::spell_check_files(rmd, ignore = ignore_spelling)
+  })
+  
+  observed <- str_c("spelling of          ", wrong_spelling$word, "        in ", wrong_spelling$found)
+  expected <- character(length = length(observed))
+  
+  write_lines(observed, "../test_logs.txt", append = TRUE)
+  expect_equal(observed, expected)
+})
+
+read_lines("../data/spelling_exceptions.txt") %>% 
+  sort() %>% 
+  write_lines("../data/spelling_exceptions.txt")
