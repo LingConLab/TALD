497e672cafc86f9814b4efed617cfaff72ff2e73
George Moroz <agricolamz@gmail.com>
2024-07-04 00:12:48 +0300

    update

--- code/test_and_write_logs.R
+++ code/test_and_write_logs.R
@@ -2,7 +2,7 @@ file.remove("test_logs.txt")
 testthat::test_dir("tests", stop_on_failure = FALSE)
 suppressPackageStartupMessages(library(tidyverse))
 test_logs <- read_lines("test_logs.txt")
-read_tsv("data/tald_villages.csv") |> 
+read_tsv("data/tald_villages.csv", progress = FALSE, show_col_types = FALSE) |> 
   select(dialect_toplevel, dialect_nt1, dialect_nt2, dialect_nt3, village_dialect) |> 
   pivot_longer(names_to = "col", values_to = "value", everything()) |> 
   na.omit() |> 
