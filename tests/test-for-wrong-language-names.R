baa59dc27bddb65b215d209d0b09a018c56b5276
George Moroz <agricolamz@gmail.com>
2024-09-21 00:57:16 +0300

    new tald_villages.csv

--- tests/test-for-wrong-language-names.R
+++ tests/test-for-wrong-language-names.R
@@ -24,7 +24,7 @@ match with `../data/tald_villages.csv`", {
     wrong_langs_df
 
   if(nrow(wrong_langs_df) > 0){
-    observed <- str_c("The wrong langage name          ", 
+    observed <- str_c("The wrong language name          ", 
                       wrong_langs_df$lang, 
                       "          in the file          ",
                       wrong_langs_df$file)
